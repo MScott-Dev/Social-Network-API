c92eaa8fdd7b4e71f4f1f35a296bd30f68368e1f
MScott-Dev <MScott0199@gmail.com>
2024-02-13 16:31:50 -0500

    Added user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const validator = require("validator");
+const thoughtSchema = require("./Thought");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "invalid email"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+// Create a virtual property `friendCount` that gets the amount of comments per post
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
