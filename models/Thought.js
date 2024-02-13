c27f3765f0f245210d1d63b769bba012837a4eaa
MScott-Dev <MScott0199@gmail.com>
2024-02-13 18:22:17 -0500

    Added thought model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reactions");
+
+
+// Schema to create thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //   TODO : Use a getter method to format the timestamp on query
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+// Create a virtual property `reactionCount` that gets the amount of comments per post
+userSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Initialize our User model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -20,19 +20,18 @@ const userSchema = new Schema(
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Thought",
+        ref: "thought",
       },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: "User",
+        ref: "user",
       },
     ],
   },
   {
     toJSON: {
-      getters: true,
       virtuals: true,
     },
   }
