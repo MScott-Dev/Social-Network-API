60fbee7036cb64c3444fbc3581eec3cefeda52e0
MScott-Dev <MScott0199@gmail.com>
2024-02-15 23:24:18 -0500

    Added get routes for all thoughts and a single thought by id

---
+++ controllers/thoughtController.js
@@ -0,0 +1,33 @@
+const { ObjectId } = require("mongoose").Types;
+const { Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+
+      res.json(thoughts);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // get a single Thought
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({
+        _id: req.params.thoughtId,
+      }).select("-__v");
+
+      if (!user) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+}
