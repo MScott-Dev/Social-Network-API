585077105a7bc0b57df38a10eb6a15ab66b25054
MScott-Dev <MScott0199@gmail.com>
2024-02-15 23:38:34 -0500

    Added route  for posting a thought and conencting it to the user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require("mongoose").Types;
-const { Thought } = require("../models");
+const { Thought, User } = require("../models");
 
 module.exports = {
   // Get all thoughts
@@ -20,7 +20,7 @@ module.exports = {
         _id: req.params.thoughtId,
       }).select("-__v");
 
-      if (!user) {
+      if (!thought) {
         return res.status(404).json({ message: "No thought with that ID" });
       }
 
@@ -30,4 +30,25 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
-}
+  // create a new thought
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+
+      const user = await User.findByIdAndUpdate(
+        { _id: req.body.userId },
+        { $addToSet: { thoughts: thought._id } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        res.status(404).json({ message: "incorrect id, please try again" });
+      }
+
+      res.json( { thought, user } );
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+};
