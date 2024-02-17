6e8c611abe757184ff9e8c10996fdc6def9bab4e
MScott-Dev <MScott0199@gmail.com>
2024-02-17 00:57:29 -0500

    added post route for reaction to a thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -90,4 +90,25 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Add an reaction to a thought
+  async addReaction(req, res) {
+
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: "No thought found with that id" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
